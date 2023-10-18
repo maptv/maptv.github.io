d9fcc94b2b92d89f3316934e6a6df19aec426292
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2023-10-18 16:02:52 -0400

    a new blog post

--- posts/post-with-code/index.py
+++ posts/post-with-code/index.py
@@ -2,14 +2,16 @@
 # title: Post With Code
 # author: Martin Laptev
 # date: now
+# cache: true
 # categories:
 #   - news
 #   - code
 #   - analysis
+#   - data visualization
 # image: image.jpg
 # jupyter:
 #   jupytext:
-#     formats: qmd,py:percent,md,ipynb
+#     formats: qmd:quarto,py:percent,md,ipynb
 #     text_representation:
 #       extension: .py
 #       format_name: percent
@@ -29,7 +31,30 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # %% [markdown]
-# The plot below is from the [Seaborn Python library documentation](https://seaborn.pydata.org/examples/horizontal_boxplot.html).
+# The plot below is from the [Seaborn Python library documentation](https://seaborn.pydata.org/examples/horizontal_boxplot.html):
+
+# %% [markdown]
+# 1. 5 blog posts, not 3
+# 2. Topics are pre-defined:
+#     1. Probability theory and random variables
+#       - Histogram
+#     2. Clustering
+#       - DBSCAN labels for scatter plot
+#     3. Linear and nonlinear regression
+#       - line on scatter plot
+#     4. Classification
+#       - ROC, PR, Confusion Matrix
+#     5. Anomaly/outlier detection
+#       - DBSCAN labels for scatter plot
+
+# %% [markdown]
+# Learning objectives:
+# 1. Use various techniques related to preprocessing prior to the use of machine learning models.
+# 2. Describe the probability theory and random variables.
+# 3. Identify the common tasks in machine learning/data mining models for clustering.
+# 4. Analyze multiple linear and nonlinear regression.
+# 5. Describe the algorithms, theories, and applications related to machine learning/data mining for classification.
+# 6. Detect anomaly/outlier behavior and the treatment techniques.
 
 # %%
 import seaborn as sns
@@ -59,7 +84,7 @@ ax.set(ylabel="")
 sns.despine(trim=True, left=True)
 
 # %% [markdown]
-# The plot below is from the [Yellowbrick Python library documentation](https://www.scikit-yb.org/en/latest/api/features/jointplot.html?highlight=joint%20plot#joint-plot-visualization).
+# The plot below is from the [Yellowbrick Python library documentation](https://www.scikit-yb.org/en/latest/api/features/jointplot.html?highlight=joint%20plot#joint-plot-visualization):
 
 # %%
 from yellowbrick.datasets import load_concrete
@@ -73,3 +98,11 @@ visualizer = JointPlotVisualizer(columns="cement")
 
 visualizer.fit_transform(X, y)        # Fit and transform the data
 visualizer.show()                     # Finalize and render the figure
+
+# %% [markdown]
+# The example below shows tuning of the regularization strength $\alpha$.
+# ![image.png](attachment:3f6e174e-eca2-47a0-bc3b-605681c448ea.png)
+# ![image.png](attachment:822e2379-4768-4f25-93c5-1ecfea0b84b7.png)
+
+# %% [markdown]
+# This is a change to the markdown text.
