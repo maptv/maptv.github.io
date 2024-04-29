017b8303c5dd3afb63a334a12717e8711e01f816
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-04-29 18:36:54 -0400

    update about page

--- scripts/time.js
+++ scripts/time.js
@@ -1,19 +1,15 @@
 // https://howardhinnant.github.io/date_algorithms.html#days_from_civil
-function dote2doty(days = 719468) {
-    const era = Math.floor((days >= 0 ? days : days - 146096) / 146097),
-        dotc = days - era * 146097,
-        yotc = Math.floor((dotc - Math.floor(dotc / 1460)
-        + Math.floor(dotc / 36524) - Math.floor(dotc / 146096)) / 365);
-    return [yotc + era * 400, dotc + Math.floor(yotc / 100)
-        - yotc * 365 - Math.floor(yotc / 4)];
-};
-function unix2doty(ms = 0) { return dote2doty(ms / 86400000 + 719468) };
+function unix2dote(unix, zone) {
+  return [(unix ?? Date.now()) / 86400000 + 719467.6 + (zone = zone ?? 4 - Math.round((new Date).getTimezoneOffset() / 144)) / 10, zone]
+}
+function dote2doty(dote = 719468, zone = 0) {
+  const cykl = Math.floor((dote >= 0 ? dote : dote - 146096) / 146097), dotc = dote - cykl * 146097,
+        yotc = Math.floor((dotc - Math.floor(dotc / 1460) + Math.floor(dotc / 36524) - Math.floor(dotc / 146096)) / 365);
+  return [yotc + cykl * 400, dotc + Math.floor(yotc / 100) - yotc * 365 - Math.floor(yotc / 4), zone];
+}
 function doty2deco(year = 1969, doty = 306, zone = 0) {
-    [date, time] = doty.toString().split(".")
-    return `${year}+${date.toString().padStart(3, "0")}.${
-    time.toString().slice(0, 5).padStart(5, "0")}`;
-};
-function unix2deco(ms = 0) { return doty2deco(...unix2doty(ms)) };
+  return `${year.toString().padStart(4, "0")}+${Math.floor(doty).toString().padStart(3, "0")}${String(doty % 1).slice(1, 7)}.${zone}`
+}
 const nbc = document.getElementsByClassName("navbar-brand-container");
 const div = document.createElement("div");
 const cal = document.createElement("i");
@@ -26,5 +22,4 @@ div.setAttribute("id", "deco");
 nbc[0].appendChild(cal);
 nbc[0].appendChild(div);
 nbc[0].appendChild(tik);
-setInterval(() => document.getElementById("deco").innerText =
-  unix2deco(Date.now()).slice(0, 14), 86.4);
+setInterval(() => document.getElementById("deco").innerText = doty2deco(...dote2doty(...unix2dote())) );
