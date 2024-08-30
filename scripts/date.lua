c1af69d1db28ac08369420fb07b1416289f9c9c5
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-08-30 00:16:54 -0400

    replacing html blocks at the bottom of the qmd file with include-after-body values in yaml frontmatter

--- scripts/date.lua
+++ scripts/date.lua
@@ -2,8 +2,8 @@ local function unix2deco(ms)
     local days = ms / 86400000 + 719468
     local era = (days >= 0 and days or days - 146096) // 146097
     local doe = days - era * 146097
-    local year = math.floor((doe - doe / 1460 + doe / 36524 - doe / 146096) / 365) + era * 400
-    local doty = days - math.floor(year * 365 + year / 4 - year / 100 + year / 400)
+    local year = (doe - doe // 1460 + doe // 36524 - doe // 146096) // 365 + era * 400
+    local doty = days - math.floor(year * 365 + year // 4 - year // 100 + year // 400)
     return string.format("%s+%s", math.floor(year), math.floor(doty))
 end
 
