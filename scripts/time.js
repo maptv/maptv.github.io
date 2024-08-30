8c053433a145c6d545663ad2f2d4c2e4f0f024be
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-08-30 00:45:59 -0400

    Built site for gh-pages

--- scripts/time.js
+++ scripts/time.js
@@ -17,23 +17,37 @@ function doty2date(year = 1969, doty = 306) {
 function doty2time(year = 1969, doty = 306, zone = 0) {
   return `${String(doty % 1 * 10).slice(0, 6)}-${zone}`
 }
-function setStamp() {
-    document.getElementById("date").innerText = doty2date(...dote2doty(...unix2dote()));
-    document.getElementById("time").innerText = doty2time(...dote2doty(...unix2dote()));
+function doty2time(year = 1969, doty = 306, zone = 0) {
+  return `${String(doty % 1 * 10).slice(0, 6)}-${zone}`
 }
+function setStamp() {
+  document.getElementById("date").innerText = doty2date(...dote2doty(...unix2dote()));
+  document.getElementById("time").innerText = doty2time(...dote2doty(...unix2dote()));
+}
+
 const nbc = document.getElementsByClassName("navbar-brand-container"),
   dat = document.createElement("div"),
   tim = document.createElement("div"),
   cal = document.createElement("i"),
-  tik = document.createElement("i");
+  tik = document.createElement("i"),
+  dot = document.createElement("i"),
+  url = new URLSearchParams(window.location.search),
+  key = url.keys().filter(v => !isNaN(v) && !isNaN(parseFloat(v)) && isFinite(v)).toArray(),
+  val = url.values().filter(v => !isNaN(v) && !isNaN(parseFloat(v)) && isFinite(v)).toArray();
+console.log("k", [...url.keys()])
+console.log("v", [...url.values()])
+console.log("v", val)
+console.log("k", key)
 cal.classList.add("bi", "bi-calendar");
 cal.setAttribute("role", "img");
 tik.classList.add("bi", "bi-clock");
 tik.setAttribute("role", "img");
 dat.setAttribute("id", "date");
 tim.setAttribute("id", "time");
+dot.setAttribute("id", "dote");
 nbc[0].appendChild(cal);
 nbc[0].appendChild(dat);
 nbc[0].appendChild(tik);
 nbc[0].appendChild(tim);
+nbc[0].appendChild(dot);
 setInterval(setStamp);
