5c112a5189a308169a438d667656e508c00a0a80
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-09-01 09:55:05 -0400

    recover blog after breaking it with ohq content

---
+++ assets/date.py
@@ -0,0 +1,21 @@
+import bs4
+
+def unix2doty(ms=0):
+    days = ms / 86400000 + 719468
+    dote = days - (cykl := (days if days >= 0 else days - 146096) // 146097) * 146097
+    year = int((dote - dote / 1460 + dote / 36524 - dote / 146096) // 365 + cykl * 400)
+    return year, days - (year * 365 + year / 4 - year / 100 + year / 400) // 1
+
+with open("_site/blog/index.html") as infile:
+    txt = infile.read()
+    soup = bs4.BeautifulSoup(txt, features="html.parser")
+for div in soup.find_all("div", {"class": "card-file-modified"}):
+    elem = div.find("p")
+    if "+" not in elem.text:
+        y, d = unix2doty(int(div.text))
+        elem.string.replace_with(f"{y:>04}+{d.__floor__():>03}")
+for div in soup.find_all("div", {"class": "listing-reading-time"}):
+    if "min" in div.text:
+        div.string.replace_with(f"{(int(div.text.split()[0]) / 1.44).__floor__()} milliday")
+with open("_site/blog/index.html", "w") as outfile:
+    outfile.write(str(soup))
