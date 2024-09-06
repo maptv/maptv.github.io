24f3021790960f7be849b7c06ba7949daaf4066c
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-09-06 02:33:27 -0400

    add markdown files to multilang dec post

--- dec/date/convert.sh
+++ dec/date/convert.sh
@@ -4,6 +4,8 @@ jupytext temp.js.ipynb --to js --output index.js --set-kernel deno
 
 jupytext index.js --to qmd --output index.js.qmd --set-kernel deno
 
+jupytext index.js --to md:myst --output index.js.md --set-kernel deno
+
 jupytext index.js --output index.js.ipynb --execute --update --set-kernel deno
 
 
@@ -13,6 +15,8 @@ jupytext temp.lua.ipynb --to lua --output index.lua --set-kernel xlua
 
 jupytext index.lua --to qmd --output index.lua.qmd --set-kernel xlua
 
+jupytext index.lua --to md:myst --output index.lua.md --set-kernel xlua
+
 jupytext index.lua --output index.lua.ipynb --execute --update --set-kernel xlua
 
 
@@ -21,5 +25,7 @@ quarto render index.qmd --profile python --cache-refresh --metadata engine:jupyt
 jupytext temp.py.ipynb --to py --output index.py --set-kernel python3
 
 jupytext index.py --to qmd --output index.py.qmd --set-kernel python3
+
+jupytext index.py --to md:myst --output index.py.md --set-kernel python3
 
 jupytext index.py --output index.py.ipynb --execute --update --set-kernel python3
