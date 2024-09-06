e1bdeb4ed5445488eb3c9bb9b8da10027a192829
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-09-06 02:00:05 -0400

    finish working example of multilang dec post

--- dec/date/convert.sh
+++ dec/date/convert.sh
@@ -4,7 +4,7 @@ jupytext temp.js.ipynb --to js --output index.js --set-kernel deno
 
 jupytext index.js --to qmd --output index.js.qmd --set-kernel deno
 
-# jupytext index.js --output index.js.ipynb --execute --update --set-kernel deno
+jupytext index.js --output index.js.ipynb --execute --update --set-kernel deno
 
 
 quarto render index.qmd --profile lua --cache-refresh --metadata engine:jupyter --to ipynb --output - > temp.lua.ipynb
@@ -13,7 +13,7 @@ jupytext temp.lua.ipynb --to lua --output index.lua --set-kernel xlua
 
 jupytext index.lua --to qmd --output index.lua.qmd --set-kernel xlua
 
-# jupytext index.lua --output index.lua.ipynb --execute --update --set-kernel xlua
+jupytext index.lua --output index.lua.ipynb --execute --update --set-kernel xlua
 
 
 quarto render index.qmd --profile python --cache-refresh --metadata engine:jupyter --to ipynb --output - > temp.py.ipynb
@@ -22,4 +22,4 @@ jupytext temp.py.ipynb --to py --output index.py --set-kernel python3
 
 jupytext index.py --to qmd --output index.py.qmd --set-kernel python3
 
-# jupytext index.py --output index.py.ipynb --execute --update --set-kernel python3
+jupytext index.py --output index.py.ipynb --execute --update --set-kernel python3
