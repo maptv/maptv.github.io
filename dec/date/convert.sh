32ec39dcd71440e8766c172b2812488cade880f7
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-09-06 02:59:01 -0400

    try to use percent format for scripts in multilang dec post

--- dec/date/convert.sh
+++ dec/date/convert.sh
@@ -11,7 +11,7 @@ jupytext index.js --output index.js.ipynb --execute --update --set-kernel deno
 
 quarto render index.qmd --profile lua --cache-refresh --metadata engine:jupyter --to ipynb --output - > temp.lua.ipynb
 
-jupytext temp.lua.ipynb --to lua --output index.lua --set-kernel xlua
+jupytext temp.lua.ipynb --to lua:percent --output index.lua --set-kernel xlua
 
 jupytext index.lua --to qmd --output index.lua.qmd --set-kernel xlua
 
@@ -22,7 +22,7 @@ jupytext index.lua --output index.lua.ipynb --execute --update --set-kernel xlua
 
 quarto render index.qmd --profile python --cache-refresh --metadata engine:jupyter --to ipynb --output - > temp.py.ipynb
 
-jupytext temp.py.ipynb --to py --output index.py --set-kernel python3
+jupytext temp.py.ipynb --to py:percent --output index.py --set-kernel python3
 
 jupytext index.py --to qmd --output index.py.qmd --set-kernel python3
 
