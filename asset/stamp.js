4b04528fad0f25867dc2e2cd5d1b1f81d2b2e583
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2024-11-03 14:10:09 -0500

    Changed files: _quarto.yml asset/stamp.js dec/date/index.qmd dec/snap/index.qmd quarto/filter/index.qmd quarto/index.qmd quarto/script/index.qmd ref.bib

--- asset/stamp.js
+++ asset/stamp.js
@@ -1,37 +1,42 @@
 // https://howardhinnant.github.io/date_algorithms.html#days_from_civil
-function unix2dote(unix, zone) {
-  return [
-    (unix ?? Date.now()) / 86400000
-    + (zone = zone ?? (
-      10 - Math.round((new Date)
-        .getTimezoneOffset() / 144)
-    ) % 10) / 10 + 719468, zone]}
-function dote2doty(dote, zone = 0) {
-  const socy = Math.floor((
+function unix2dote(unix, zone, offset = 719468) {
+  return [(unix ?? Date.now()) / 86400000 + (
+    zone = zone ?? -Math.round(
+      (new Date).getTimezoneOffset() / 144)
+    ) / 10 + offset, zone]
+}
+function dote2date(dote, zone = 0) {
+  const cote = Math.floor((
       dote >= 0 ? dote
       : dote - 146096
     ) / 146097),
-  dotc = dote - socy * 146097,
+  dotc = dote - cote * 146097,
   yotc = Math.floor((dotc
     - Math.floor(dotc / 1460)
     + Math.floor(dotc / 36524)
     - Math.floor(dotc / 146096)
   ) / 365);
   return [
-    yotc + socy * 400,
+    yotc + cote * 400,
     dotc - (yotc * 365
       + Math.floor(yotc / 4)
       - Math.floor(yotc / 100)
   ), zone]}
-function doty2date(year = 1969, doty = 306) {
-  return `${year.toString().padStart(4, "0")}+${Math.floor(doty).toString().padStart(3, "0")}`
+function dote2dotw(d = 719468) {
+  return d >= -3 ? (d + 3) % 7 : (d + 4) % 7 + 6
+}
+function doty2date(year = 1969, doty = 306, dotw = 4) {
+  dotw = Math.floor(dotw)
+  doty = Math.floor(doty)
+  doty = doty - dotw
+  return `${year.toString().padStart(4, "0")}${doty < 0 ? "-" : "+"}${Math.abs(doty).toString().padStart(3, "0")}+${dotw}`
 }
 function doty2time(doty = 306, zone = 0) {
-  return `${String(doty % 1 * 10).slice(0, 6)}-${zone}`
+  return `${(doty % 1 * 10).toFixed(4)}${zone < 0 ? "+" : "-"}${Math.abs(zone)}`
 }
 function setStamp() {
-  [y, d, z] = dote2doty(...unix2dote());
-  document.getElementById("date").innerText = doty2date(y, d);
+  const [dote, zone] = unix2dote(), [y, d, z] = dote2date(dote, zone);
+  document.getElementById("date").innerText = doty2date(y, d, dote2dotw(dote));
   document.getElementById("time").innerText = doty2time(d, z);
 }
 function isNumeric(x){
@@ -49,7 +54,7 @@ if (url.size > 0) {
   const numericEntries = Object.fromEntries(url.entries().filter(
     ([k, v]) => isNumeric(k) || k === "")
   )
-  
+
   // const key = url.keys().filter(x =>).toArray(),
   // val = url.values().filter(v => !isNaN(v) && !isNaN(parseFloat(v)) && isFinite(v)).toArray();
 
