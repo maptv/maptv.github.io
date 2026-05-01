fd1e6c0e7d2babfc63352ed6201279949602feac
Martin Skarzynski Laptev <129316885+maptv@users.noreply.github.com>
2026-04-30 23:55:25 -0400

    move lots of metadata into _quarto.yml, fix styling, and work on the cite section

--- asset/date.lua
+++ asset/date.lua
@@ -1,31 +1,58 @@
 -- https://howardhinnant.github.io/date_algorithms.html#days_from_civil
 function unix2deco(ms)
+  ms = tonumber(ms)
+  if not ms then
+    return ""
+  end
+
   local dote = ms / 86400 + 719468
+
   local cykl = (
     dote >= 0 and dote
     or dote - 146096
   ) // 146097
+
   local dotc = dote - cykl * 146097
+
   local yotc = (
     dotc - dotc // 1460
     + dotc // 36524
     - dotc // 146096
   ) // 365
+
   return string.format(
-    "%s+%03d",
+    "%d+%03d",
     math.floor(yotc + cykl * 400),
-    math.floor(dotc - (yotc * 365
-      + yotc // 4
-      - yotc // 100)))
+    math.floor(
+      dotc - (
+        yotc * 365
+        + yotc // 4
+        - yotc // 100
+      )
+    )
+  )
 end
 
-local function to_decalendar(date)
-  local date = pandoc.utils.stringify(date)
+local function to_decalendar(date_meta)
+  if not date_meta then
+    return date_meta
+  end
+
+  local date = pandoc.utils.stringify(date_meta)
   local unix = date:match("(%d+)")
-  return date:gsub(unix, unix2deco(unix))
+
+  if not unix then
+    return date_meta
+  end
+
+  local converted = date:gsub(unix, unix2deco(unix))
+
+  return pandoc.MetaString(converted)
 end
 
 function Meta(m)
-  m.date = to_decalendar(m.date)
+  if m.date then
+    m.date = to_decalendar(m.date)
+  end
   return m
 end
